b705def390427fecf800bfb2d34f6a033a4ab892
Jude Rajasekera <juderajasekera@gmail.com>
2019-12-22 14:11:26 -0500

    add background and landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
     skip_before_action :authenticate_user!
-    def test
+
+    def info
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/home/test" => "home#test"
+  get "/home/info" => "home#info"
 
-  root 'home#test'
-  #root 'users#sign_in'
+  root 'home#info'
 
 end
