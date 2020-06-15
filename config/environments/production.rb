19b3e340c96e77742a3fb568ca847da196cde0d1
Jude Rajasekera <juderajasekera@gmail.com>
2020-06-14 23:38:46 -0400

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,14 @@ Rails.application.configure do
   #    :domain  => 'yourdomain.com'
   #}
 
+  ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'immense-thicket-26869.herokuapp.com',
+      :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
 end
