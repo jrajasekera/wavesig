237b47e287712429ac8d2a967bf2a1c1172cb110
Jude Rajasekera <juderajasekera@gmail.com>
2020-07-04 11:04:02 -0400

    Add debug method for env vars

--- config/deploy.rb
+++ config/deploy.rb
@@ -86,3 +86,12 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
+
+namespace :debug do
+  desc 'Print ENV variables'
+  task :env do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :printenv
+    end
+  end
+end
