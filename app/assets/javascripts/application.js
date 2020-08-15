10980ffbfb5eb5f3ee036bc27bd533f8501f24ce
Jude Rajasekera <juderajasekera@gmail.com>
2020-08-15 17:05:39 -0400

    Redo music player

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,51 @@
 //= require Chart.bundle
 //= require local-time
 
+let playing = false;
+
 $(document).ready(function() {
+
+    $(".progress").hover(function(){
+        document.getElementById("current-playback-time").style.display = "none";
+        if(playing) {
+            document.getElementById('pause-music-btn').style.display = "initial";
+        } else {
+            document.getElementById('play-music-btn').style.display = "initial";
+        }
+    }, function(){
+        document.getElementById("current-playback-time").style.display = "initial";
+        if(playing) {
+            document.getElementById('pause-music-btn').style.display = "none";
+        } else {
+            document.getElementById('play-music-btn').style.display = "none";
+        }
+    });
+
+    let player = document.getElementById('player');
+
+
+
+    player.addEventListener("loadedmetadata", function(event) {
+        document.getElementById('file-duration').innerText = player.duration.toString().toMMSS();
+    });
+
+    player.addEventListener("timeupdate", function(event) {
+        let duration = player.duration;
+        let currentTime = player.currentTime;
+        let completionPercentage = Math.round((currentTime / duration) * 100);
+        document.getElementById('progress-circle').setAttribute("data-value",completionPercentage.toString());
+        setProgress();
+        document.getElementById("current-playback-time").innerText = currentTime.toString().toMMSS();
+        // console.log(completionPercentage);
+    });
+
+    player.addEventListener("ended", function(event) {
+        playing = false;
+        document.getElementById('progress-circle').setAttribute("data-value","0");
+        setProgress();
+        document.getElementById("current-playback-time").innerText = "00:00";
+    });
+
 });
 
 $(document).on('turbolinks:load', function () {
@@ -32,6 +76,7 @@ $(document).on('turbolinks:load', function () {
         enableCaseInsensitiveFiltering: true,
         nonSelectedText: 'Select friends'
     });
+
 });
 
 $(document).on('turbolinks:load', function(){
@@ -124,3 +169,52 @@ document.addEventListener("notifications-count-loaded", function(event) {
         iconTag.attr("data-count",notificationsDisplayCount);
     }
 });
+
+function setProgress() {
+    let progressCircle = document.getElementById('progress-circle');
+    var value = $(progressCircle).attr('data-value');
+    var left = $(progressCircle).find('.progress-left .progress-bar');
+    var right = $(progressCircle).find('.progress-right .progress-bar');
+
+    if (value > 0) {
+        if (value <= 50) {
+            right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+        } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        }
+    } else {
+        right.css('transform', 'rotate(0deg)');
+        left.css('transform', 'rotate(0deg)');
+    }
+}
+
+function percentageToDegrees(percentage) {
+    return percentage / 100 * 360
+}
+
+String.prototype.toMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return minutes + ':' + seconds;
+}
+function clickPlay() {
+    playing = true;
+    document.getElementById('player').play();
+    document.getElementById('play-music-btn').style.display = "none";
+    document.getElementById('pause-music-btn').style.display = "initial";
+}
+
+function clickPause() {
+    playing = false;
+    document.getElementById('player').pause();
+    document.getElementById('pause-music-btn').style.display = "none";
+    document.getElementById('play-music-btn').style.display = "initial";
+}
+
